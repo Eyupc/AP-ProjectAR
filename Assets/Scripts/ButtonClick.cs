b7e64ba73e1f5bfd3c6cb69fdf38d7955c5f2936
TaneeKempenaers <37657877+TaneeKempenaers@users.noreply.github.com>
2024-11-21 17:26:12 +0100

    fix navigation from navbar to dynamically change between screens

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class ButtonClick : MonoBehaviour
 {
-    public GameObject targetCanvas;
+    public GameObject homeCanvas;
+    public GameObject cameraCanvas;
 
-    // Functie om het canvas aan of uit te zetten
-    public void ToggleCanvasVisibility()
+    public void ShowUI(string target)
     {
-        if (targetCanvas != null)
+        homeCanvas.SetActive(false);
+        cameraCanvas.SetActive(false);
+
+        switch (target)
         {
-            // Wissel tussen aan en uit
-            bool isActive = targetCanvas.activeSelf;
-            targetCanvas.SetActive(!isActive);
-            Debug.LogWarning("boe");
-        }
-        else
-        {
-            Debug.LogWarning("Er is geen canvas gekoppeld aan dit script!");
+            case "Home":
+                homeCanvas.SetActive(true);
+                cameraCanvas.SetActive(false);
+                Debug.Log("Home canvas is now visible.");
+                break;
+
+            case "Camera":
+                cameraCanvas.SetActive(true);
+                homeCanvas.SetActive(false);
+                Debug.Log("Camera canvas is now visible.");
+                break;
+
+            default:
+                Debug.LogWarning("Invalid target: " + target);
+                break;
         }
     }
+    //public GameObject targetCanvas;
+    //public void ToggleCanvasVisibility()
+    //{
+    //    if (targetCanvas != null)
+    //    {
+    //        targetCanvas.SetActive(false);
+    //        Debug.LogWarning(targetCanvas+" is clicked");
+    //    }
+    //    else
+    //    {
+    //        Debug.LogWarning("Er is geen canvas gekoppeld aan dit script!");
+    //    }
+    //}
 }
