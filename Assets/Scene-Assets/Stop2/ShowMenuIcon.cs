d1c5233543d1a9ef5d9ee806d2c4206f0f7c8fa8
TaneeKempenaers <37657877+TaneeKempenaers@users.noreply.github.com>
2024-12-29 21:21:59 +0100

    make sure right prefab is shown based on language option

--- Assets/Scene-Assets/Stop2/ShowMenuIcon.cs
+++ Assets/Scene-Assets/Stop2/ShowMenuIcon.cs
@@ -6,11 +6,14 @@ public class ShowMenuIcon : QRActionBase
     [SerializeField] private GameObject menuIconCanvasPrefab;
     [SerializeField] private GameObject welcomeOverlayPrefab;
     [SerializeField] private GameObject avatarPrefab;
+    [SerializeField] private GameObject avatarArabicPrefab;
+    private Language language = new Language();
+    private Character character = new Character();
     public override string QRCodeText => "Restaurant";
 
     public override void Execute(Vector3 position, Quaternion rotation)
     {
-        if (menuIconCanvasPrefab != null && welcomeOverlayPrefab != null && avatarPrefab != null)
+        if (menuIconCanvasPrefab != null && welcomeOverlayPrefab != null && avatarPrefab != null && avatarArabicPrefab != null)
         {
             GameObject overlayObj = Instantiate(welcomeOverlayPrefab);
             overlayObj.GetComponent<StopStartCanvasHandler>().OnCloseClicked += SpawnMenuIcon;
@@ -29,6 +32,8 @@ public class ShowMenuIcon : QRActionBase
 
     private void SpawnAvatar() 
     {
+        language = UserSystemManager.Language;
+        character = UserSystemManager.Character;
         Camera mainCamera = Camera.main;
         if (mainCamera != null)
         {
@@ -36,9 +41,14 @@ public class ShowMenuIcon : QRActionBase
             Vector3 rightDirection = mainCamera.transform.right;
             Vector3 spawnPosition = userPosition + rightDirection * -2f;
             spawnPosition.y -= 1.7f;
+            GameObject avatar = null;
+            if (language == Language.Dutch) { avatar = Instantiate(avatarPrefab, spawnPosition, Quaternion.identity); }
+            else { avatar = Instantiate(avatarArabicPrefab, spawnPosition, Quaternion.identity); }
 
-            GameObject avatar = Instantiate(avatarPrefab, spawnPosition, Quaternion.identity);
-            avatar.transform.LookAt(new Vector3(userPosition.x, avatar.transform.position.y, userPosition.z));
+            if (avatar != null)
+            {
+                avatar.transform.LookAt(new Vector3(userPosition.x, avatar.transform.position.y, userPosition.z));
+            }
         }
     }
 }
