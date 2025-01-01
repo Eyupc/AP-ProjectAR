35278ce94297fbb417c530eacb1f5eb933624b4c
TaneeKempenaers <37657877+TaneeKempenaers@users.noreply.github.com>
2025-01-01 14:40:46 +0100

    finish avatars from stop2

--- Assets/Scene-Assets/Stop2/Scripts/ShowMenuIcon.cs
+++ Assets/Scene-Assets/Stop2/Scripts/ShowMenuIcon.cs
@@ -7,6 +7,8 @@ public class ShowMenuIcon : QRActionBase
     [SerializeField] private GameObject welcomeOverlayPrefab;
     [SerializeField] private GameObject avatarPrefab;
     [SerializeField] private GameObject avatarArabicPrefab;
+    [SerializeField] private GameObject avatarWomanPrefab;
+    [SerializeField] private GameObject avatarWomanArabicPrefab;
     private Language language = new Language();
     private Character character = new Character();
     public override string QRCodeText => "Restaurant";
@@ -40,8 +42,16 @@ public class ShowMenuIcon : QRActionBase
             Vector3 spawnPosition = userPosition + rightDirection * -2f;
             spawnPosition.y -= 1.7f;
             GameObject avatar = null;
-            if (language == Language.Dutch) { avatar = Instantiate(avatarPrefab, spawnPosition, Quaternion.identity); }
-            else { avatar = Instantiate(avatarArabicPrefab, spawnPosition, Quaternion.identity); }
+            if (language == Language.Dutch)
+            {
+                if(character == Character.Man) { avatar = Instantiate(avatarPrefab, spawnPosition, Quaternion.identity); }
+                else { avatar = Instantiate(avatarWomanPrefab, spawnPosition, Quaternion.identity); }
+            }
+            else 
+            {
+                if (character == Character.Man) { avatar = Instantiate(avatarArabicPrefab, spawnPosition, Quaternion.identity); }
+                else { avatar = Instantiate(avatarWomanArabicPrefab, spawnPosition, Quaternion.identity); }
+            }
 
             avatar.transform.LookAt(new Vector3(userPosition.x, avatar.transform.position.y, userPosition.z));
             avatar.GetComponent<PlayPoem>().OnPoemEnd += () =>
