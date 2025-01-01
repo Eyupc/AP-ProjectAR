4f1b23936750d0dd960474ee334bf9533d10b9d5
TaneeKempenaers <37657877+TaneeKempenaers@users.noreply.github.com>
2025-01-01 14:15:00 +0100

    finish avatars from stop3

--- Assets/Scene-Assets/Stop3/SpawnAvatarAction3.cs
+++ Assets/Scene-Assets/Stop3/SpawnAvatarAction3.cs
@@ -6,6 +6,8 @@ public class SpawnAvatarAction3 : QRActionBase
 {
     [SerializeField] private GameObject avatarPrefab;
     [SerializeField] private GameObject avatarArabicPrefab;
+    [SerializeField] private GameObject avatarWomanPrefab;
+    [SerializeField] private GameObject avatarWomanArabicPrefab;
     private Language language = new Language();
     private Character character = new Character();
     public override string QRCodeText => "AleppoSoap";
@@ -23,8 +25,16 @@ public class SpawnAvatarAction3 : QRActionBase
             spawnPosition.y -= 1.7f;
 
             GameObject avatar = null;
-            if (language == Language.Dutch) { avatar = Instantiate(avatarPrefab, spawnPosition, Quaternion.identity); }
-            else { avatar = Instantiate(avatarArabicPrefab, spawnPosition, Quaternion.identity); }
+            if (language == Language.Dutch)
+            {
+                if (character == Character.Man) { avatar = Instantiate(avatarPrefab, spawnPosition, Quaternion.identity); }
+                else { avatar = Instantiate(avatarWomanPrefab, spawnPosition, Quaternion.identity); }
+            }
+            else 
+            {
+                if (character == Character.Man) { avatar = Instantiate(avatarArabicPrefab, spawnPosition, Quaternion.identity); }
+                else { avatar = Instantiate(avatarWomanArabicPrefab, spawnPosition, Quaternion.identity); }
+            }
 
             if (avatar != null)
             {
